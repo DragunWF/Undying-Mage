2c7b3e10d141d102b812126ae27956c5df7699fd
DragonWF <plarisanmarc@gmail.com>
2022-05-23 00:52:07 +0800

    Implemented basic movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    const float moveSpeed = 5.5f;
+    const float jumpForce = 50;
+
+    Vector2 rawInput;
+
     void Start()
     {
 
@@ -11,6 +17,17 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        Move();
+    }
 
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+    }
+
+    void Move()
+    {
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        transform.position += delta;
     }
 }
