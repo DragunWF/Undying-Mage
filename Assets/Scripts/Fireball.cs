81568685c2d622c5b68a6d19fa34c928a18ca2ff
DragonWF <plarisanmarc@gmail.com>
2022-05-23 12:31:47 +0800

    Implemented main fireball functionality

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    const float baseSpeed = 7.5f;
+    float moveSpeed;
+
+    Player player;
+    Rigidbody2D rigidBody;
+
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<Player>();
 
+        if (player.IsFacingRight)
+        {
+            moveSpeed = baseSpeed;
+            transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            moveSpeed = -baseSpeed;
+            transform.localScale = new Vector2(-1, 1);
+        }
     }
 
     void Update()
     {
-
+        rigidBody.velocity = new Vector2(moveSpeed, 0);
     }
 }
