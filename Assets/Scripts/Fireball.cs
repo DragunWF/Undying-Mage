2314c6b3edab29aeda632d73e239672cf3445536
DragonWF <plarisanmarc@gmail.com>
2022-05-25 01:00:20 +0800

    Implemented extinguishing for optimization

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Fireball : MonoBehaviour
 {
     const float baseSpeed = 9.5f;
+    const float fireballLifespan = 7.5f;
     float moveSpeed;
 
     Player player;
@@ -25,10 +26,24 @@ public class Fireball : MonoBehaviour
             moveSpeed = -baseSpeed;
             transform.localScale = new Vector2(-1, 1);
         }
+
+        StartCoroutine(ExtinguishFlames());
     }
 
     void Update()
     {
         rigidBody.velocity = new Vector2(moveSpeed, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+            Destroy(gameObject);
+    }
+
+    IEnumerator ExtinguishFlames()
+    {
+        yield return new WaitForSecondsRealtime(fireballLifespan);
+        Destroy(gameObject);
     }
 }
