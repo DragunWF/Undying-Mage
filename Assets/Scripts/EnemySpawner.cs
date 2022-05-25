3aa95e8b7ecbe8e330d6156c25853f28a0803003
DragonWF <plarisanmarc@gmail.com>
2022-05-26 00:52:54 +0800

    Implemented main spawning functionality

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,5 +4,54 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    
+    GameObject[] enemies;
+
+    Vector2 rightPos;
+    Vector2 leftPos;
+
+    float spawnIntervalMin = 0.5f;
+    float spawnIntervalMax = 1.5f;
+
+    void Awake()
+    {
+        enemies = new GameObject[2] {
+            Resources.Load("Prefabs/Enemy [Fighter]") as GameObject,
+            Resources.Load("Prefabs/Enemy [Spider]") as GameObject
+        };
+        rightPos = GameObject.Find("RightPosition").transform.position;
+        leftPos = GameObject.Find("LeftPosition").transform.position;
+    }
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    float GetSpawnInterval()
+    {
+        return Random.Range(spawnIntervalMin, spawnIntervalMax);
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        var positions = new Vector2[2] { leftPos, rightPos };
+        return positions[Random.Range(0, positions.Length)];
+    }
+
+    GameObject ChooseRandomEnemy()
+    {
+        return enemies[Random.Range(0, enemies.Length)];
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            Instantiate(ChooseRandomEnemy(),
+                        GetRandomPosition(),
+                        Quaternion.identity);
+
+            yield return new WaitForSecondsRealtime(GetSpawnInterval());
+        }
+    }
 }
