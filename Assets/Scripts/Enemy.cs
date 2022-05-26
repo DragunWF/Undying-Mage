3198e10ac1afb1eaaeb8133c63a812b0b6cd7bd9
DragonWF <plarisanmarc@gmail.com>
2022-05-27 03:01:36 +0800

    Implemented functionality for flying enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,15 +7,23 @@ public class Enemy : MonoBehaviour
     [Header("Enemy Stats")]
     [SerializeField] int health = 50;
     [SerializeField] int damage = 15;
-    [SerializeField] float speed = 1.1f;
+    [SerializeField] float moveSpeed = 1.1f;
 
     [Header("Score Gain")]
     [SerializeField] int minScoreGain = 10;
     [SerializeField] int maxScoreGain = 50;
 
+    [Header("Enemy Type")]
+    [SerializeField] bool isFlyingEnemy;
+
     PlayerState playerState;
     PlayerMovement playerMovement;
     int playerDamage;
+
+    // Only for flying enemies
+    Rigidbody2D rigidBody;
+    int lifeSpan;
+    int xAxis;
 
     public void DamageHealth()
     {
@@ -34,20 +42,49 @@ public class Enemy : MonoBehaviour
         playerDamage = playerState.FireballDamage;
     }
 
+    void Start()
+    {
+        if (isFlyingEnemy)
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+            PickFlyingDirection();
+            FlipSprite(playerMovement.GetPosition().x);
+        }
+    }
+
     void Update()
     {
-        var playerPosX = playerMovement.GetPosition().x;
-        transform.position = Vector2.MoveTowards(transform.position,
-                                                 new Vector2(playerPosX,
-                                                            transform.position.y),
-                                                 speed * Time.deltaTime);
-        FlipSprite(playerPosX);
+        Move();
+    }
+
+    void Move()
+    {
+        if (!isFlyingEnemy)
+        {
+            var playerPosX = playerMovement.GetPosition().x;
+            transform.position = Vector2.MoveTowards(transform.position,
+                                                    new Vector2(playerPosX,
+                                                                transform.position.y),
+                                                    moveSpeed * Time.deltaTime);
+            FlipSprite(playerPosX);
+        }
+        else
+        {
+            var speed = Time.deltaTime * moveSpeed * xAxis;
+            rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+        }
     }
 
     void FlipSprite(float playerPosX)
     {
         transform.localScale = playerPosX >= transform.position.x ?
                                new Vector2(1, 1) : new Vector2(-1, 1);
+    }
+
+    void PickFlyingDirection()
+    {
+        var playerPosX = playerMovement.GetPosition().x;
+        xAxis = playerPosX >= transform.position.x ? 1 : -1;
     }
 
     int GetScore()
