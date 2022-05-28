d166f21d9065f1fcd1be898d9bef7180a998da98
DragonWF <plarisanmarc@gmail.com>
2022-05-29 00:45:28 +0800

    Implemented main functionality and stats

--- Assets/Scripts/GameInfo.cs
+++ Assets/Scripts/GameInfo.cs
@@ -4,15 +4,90 @@ using UnityEngine;
 
 public class GameInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int damageLevel = 1;
+    private int fireRateLevel = 1;
+    private int acrobaticsLevel = 1; // Movement and jumping
+
+    private float statIncreasePercentage = 0.25f;
+    private float enemySpawnTimePercentage = 1;
+
+    public int Deaths { get; private set; }
+
+    #region Multiplier Getter Methods
+
+    public float GetDamageMultiplier()
     {
-        
+        return GetMultipler("damage");
     }
 
-    // Update is called once per frame
-    void Update()
+    public float GetFireRateMultiplier()
     {
-        
+        return GetMultipler("firerate");
+    }
+
+    public float GetAcrobaticsMultiplier()
+    {
+        return GetMultipler("acrobatics");
+    }
+
+    #endregion
+
+    #region Incrementor Setter Methods
+
+    public void IncrementDamageLevel()
+    {
+        damageLevel++;
+    }
+
+    public void IncrementFireRateLevel()
+    {
+        fireRateLevel++;
+    }
+
+    public void IncrementAcrobaticsLevel()
+    {
+        acrobaticsLevel++;
+    }
+
+    public void IncrementDeaths()
+    {
+        Deaths++;
+    }
+
+    #endregion
+
+    public void ResetGame()
+    {
+        Deaths = 0;
+        damageLevel = 1;
+        fireRateLevel = 1;
+        acrobaticsLevel = 1;
+    }
+
+    private void Awake()
+    {
+        ResetGame();
+    }
+
+    private float GetMultipler(string type)
+    {
+        int baseLevel;
+
+        switch (type.ToLower())
+        {
+            case "damage":
+                baseLevel = damageLevel;
+                break;
+            case "firerate":
+                baseLevel = fireRateLevel;
+                break;
+            case "acrobatics":
+                baseLevel = acrobaticsLevel;
+                break;
+            default:
+                throw new System.Exception("Unknown Stat Type Entered");
+        }
+
+        return baseLevel * statIncreasePercentage;
     }
 }
