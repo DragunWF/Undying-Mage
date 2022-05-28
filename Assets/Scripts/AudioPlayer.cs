cb158eb120330eec055318dad31665501f394573
DragonWF <plarisanmarc@gmail.com>
2022-05-28 22:39:53 +0800

    Implemented main functionality for audio plays

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class AudioPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    AudioClip playerDamaged;
+    const float playerDamagedVolume = 0.3f;
+
+    void Awake()
     {
-        
+        playerDamaged = Resources.Load("Audio/Damage") as AudioClip;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayPlayerDamaged()
     {
-        
+        PlayClip(playerDamaged, playerDamagedVolume);
+    }
+
+    void PlayClip(AudioClip clip, float volume)
+    {
+        if (clip != null)
+        {
+            Vector3 cameraPos = Camera.main.transform.position;
+            AudioSource.PlayClipAtPoint(clip, cameraPos, volume);
+        }
     }
 }
