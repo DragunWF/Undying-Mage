d2dd198adf8a54fa1c124bb6a461afdf08d01637
DragonWF <plarisanmarc@gmail.com>
2022-05-29 15:03:21 +0800

    Implemented main fields and methods

--- Assets/Scripts/UpgradeMenuUI.cs
+++ Assets/Scripts/UpgradeMenuUI.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UpgradeMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TextMeshProUGUI scoreText;
+    private TextMeshProUGUI deathsText;
+
+    private TextMeshProUGUI acrobaticsLevel;
+    private TextMeshProUGUI fireRateLevel;
+    private TextMeshProUGUI damageLevel;
+
+    private GameInfo gameInfo;
+
+    public void UpgradeAcrobatics()
     {
-        
+        gameInfo.IncrementDamageLevel();
+        SetTextUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpgradeFireRate()
     {
-        
+        gameInfo.IncrementFireRateLevel();
+        SetTextUI();
+    }
+
+    public void UpgradeDamageLevel()
+    {
+        gameInfo.IncrementDamageLevel();
+        SetTextUI();
+    }
+
+    private void Awake()
+    {
+        acrobaticsLevel = GameObject.Find("AcrobaticsLevel").GetComponent<TextMeshProUGUI>();
+        fireRateLevel = GameObject.Find("FireRateLevel").GetComponent<TextMeshProUGUI>();
+        damageLevel = GameObject.Find("DamageLevel").GetComponent<TextMeshProUGUI>();
+
+        gameInfo = FindObjectOfType<GameInfo>();
+        SetTextUI();
+    }
+
+    private void SetTextUI()
+    {
+        acrobaticsLevel.text = string.Format("Acrobatics: Lvl {0}");
+        fireRateLevel.text = string.Format("Firerate: Lvl {0}");
+        damageLevel.text = string.Format("Damage: Lvl {0}");
     }
 }
