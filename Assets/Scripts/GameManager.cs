aab26661e59ec310824b5da865918205f40b4e62
DragonWF <plarisanmarc@gmail.com>
2022-05-29 14:37:45 +0800

    Implemented main methods to load scenes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private const int mainMenuSceneIndex = 0;
+    private const int mainGameSceneIndex = 1;
+    private const int upgradeMenuSceneIndex = 2;
 
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuSceneIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGame()
     {
+        SceneManager.LoadScene(mainGameSceneIndex);
+    }
 
+    public void LoadUpgradeMenu()
+    {
+        SceneManager.LoadScene(upgradeMenuSceneIndex);
     }
 }
