18eeba762d5a91295823bd9675d74fc35f4ae776
DragonWF <plarisanmarc@gmail.com>
2022-05-30 19:45:14 +0800

    Implemented fade to black transition when loading to upgrade menu

--- Assets/Scripts/FadeToBlack.cs
+++ Assets/Scripts/FadeToBlack.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeToBlack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool transitionInitialized;
+    private const float transitionDelay = 0.25f;
+    private Image blackScreen;
+
+    private float alphaSlider = 0;
+    private const float fadeSpeed = 150.5f;
+    private float loadSceneDelay = Mathf.Ceil(255 / fadeSpeed) + 1.5f;
+
+    public void InitializeFade()
     {
-        
+        Invoke("FadeScreen", transitionDelay);
+        StartCoroutine(CallLoadScene());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        blackScreen = GameObject.Find("Black").GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if (transitionInitialized)
+        {
+            alphaSlider += fadeSpeed * Time.deltaTime;
+            alphaSlider = Mathf.Clamp(alphaSlider, 0, 255);
+            blackScreen.color = new Color(0, 0, 0, alphaSlider / 255);
+        }
+    }
+
+    private void FadeScreen()
+    {
+        transitionInitialized = true;
+    }
+
+    private IEnumerator CallLoadScene()
+    {
+        yield return new WaitForSeconds(loadSceneDelay);
+        FindObjectOfType<GameManager>().LoadUpgradeMenu();
     }
 }
