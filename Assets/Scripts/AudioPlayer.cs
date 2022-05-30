e2dca77b15981a9e1f148e4d153910b0aa950781
DragonWF <plarisanmarc@gmail.com>
2022-05-30 23:09:00 +0800

    Added declartions and public methods for two new audio clips

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -7,6 +7,12 @@ public class AudioPlayer : MonoBehaviour
     private AudioClip playerDamaged;
     private const float playerDamagedVolume = 0.3f;
 
+    private AudioClip jump;
+    private const float jumpVolume = 0.4f;
+
+    private AudioClip shoot;
+    private const float shootVolume = 0.5f;
+
     private AudioClip buttonClick;
     private const float buttonClickVolume = 1.25f;
 
@@ -21,6 +27,16 @@ public class AudioPlayer : MonoBehaviour
     public void PlayPlayerDamaged()
     {
         PlayClip(playerDamaged, playerDamagedVolume);
+    }
+
+    public void PlayJump()
+    {
+        PlayClip(jump, jumpVolume);
+    }
+
+    public void PlayShoot()
+    {
+        PlayClip(shoot, shootVolume);
     }
 
     public void PlayButtonClick()
@@ -43,6 +59,8 @@ public class AudioPlayer : MonoBehaviour
     private void Awake()
     {
         playerDamaged = Resources.Load("Audio/Damage") as AudioClip;
+        jump = Resources.Load("Audio/Jump") as AudioClip;
+        shoot = Resources.Load("Audio/Shoot") as AudioClip;
         buttonClick = Resources.Load("Audio/Click") as AudioClip;
         upgrade = Resources.Load("Audio/Upgrade") as AudioClip;
         error = Resources.Load("Audio/Error") as AudioClip;
