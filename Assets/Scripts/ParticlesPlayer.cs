005f7eecc5f1cb2b5910ed7a26a295572c2aecdd
DragonWF <plarisanmarc@gmail.com>
2022-05-30 18:07:29 +0800

    Added ParticlesPlayer.cs

---
+++ Assets/Scripts/ParticlesPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesPlayer : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
