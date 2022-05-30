066eadcc07b91204dac96643b55ce622631ad6e4
DragonWF <plarisanmarc@gmail.com>
2022-05-30 18:20:53 +0800

    Implemented play effect methods and declarations

--- Assets/Scripts/ParticlesPlayer.cs
+++ Assets/Scripts/ParticlesPlayer.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class ParticlesPlayer : MonoBehaviour
 {
-    void Start()
+    private GameObject hitEffect;
+    private GameObject deathEffect;
+    private const float effectLifespan = 2.5f;
+
+    public void PlayHitEffect(Vector2 pos)
     {
-        
+        PlayEffect(pos, hitEffect);
     }
 
-    void Update()
+    public void PlayDeathEffect(Vector2 pos)
     {
-        
+        PlayEffect(pos, deathEffect);
+    }
+
+    private void Awake()
+    {
+        hitEffect = Resources.Load("Prefabs/HitEffect") as GameObject;
+        deathEffect = Resources.Load("Prefabs/DeathEffect") as GameObject;
+    }
+
+    private void PlayEffect(Vector2 pos, GameObject effect)
+    {
+        var instance = Instantiate(effect, pos, Quaternion.identity);
+        Destroy(instance.gameObject, effectLifespan);
     }
 }
