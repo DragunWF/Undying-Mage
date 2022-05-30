7e50e1e83bd817ab87835c0b6aeb0145ba502523
DragonWF <plarisanmarc@gmail.com>
2022-05-30 12:50:23 +0800

    New audio clip references and public methods for playing them

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -7,9 +7,25 @@ public class AudioPlayer : MonoBehaviour
     private AudioClip playerDamaged;
     private const float playerDamagedVolume = 0.3f;
 
+    private AudioClip buttonClick;
+    private const float buttonClickVolume = 1.25f;
+
+    private AudioClip error;
+    private const float errorVolume = 0.85f;
+
     public void PlayPlayerDamaged()
     {
         PlayClip(playerDamaged, playerDamagedVolume);
+    }
+
+    public void PlayButtonClick()
+    {
+        PlayClip(buttonClick, buttonClickVolume);
+    }
+
+    public void PlayError()
+    {
+        PlayClip(error, errorVolume);
     }
 
     private void Awake()
