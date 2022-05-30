213d78e83f5bc6224b384ada2ea786feacd1e5fc
DragonWF <plarisanmarc@gmail.com>
2022-05-30 13:29:54 +0800

    Minor bug fixes and added upgrade sound effect

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -10,8 +10,13 @@ public class AudioPlayer : MonoBehaviour
     private AudioClip buttonClick;
     private const float buttonClickVolume = 1.25f;
 
+    private AudioClip upgrade;
+    private const float upgradeVolume = 1.25f;
+
     private AudioClip error;
     private const float errorVolume = 0.85f;
+
+    #region Audio Play Methods
 
     public void PlayPlayerDamaged()
     {
@@ -23,14 +28,24 @@ public class AudioPlayer : MonoBehaviour
         PlayClip(buttonClick, buttonClickVolume);
     }
 
+    public void PlayUpgrade()
+    {
+        PlayClip(upgrade, upgradeVolume);
+    }
+
     public void PlayError()
     {
         PlayClip(error, errorVolume);
     }
 
+    #endregion
+
     private void Awake()
     {
         playerDamaged = Resources.Load("Audio/Damage") as AudioClip;
+        buttonClick = Resources.Load("Audio/Click") as AudioClip;
+        upgrade = Resources.Load("Audio/Upgrade") as AudioClip;
+        error = Resources.Load("Audio/Error") as AudioClip;
     }
 
     private void PlayClip(AudioClip clip, float volume)
